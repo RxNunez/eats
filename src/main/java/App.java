268e3fdbfb9575cdb55ba5aef88e3ccc154e42e6
Esti Shay and Kheifer Fuller <esti@>
2017-08-10 12:01:07 -0700

    add new REST routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,6 +17,24 @@ public class App {
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
+        get("/restaurants/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Restaurant place = Restaurant.findById(Integer.parseInt(request.params(":id")));
+            model.put("place", place);
+            return new ModelAndView(model, "restaurant-detail.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/restaurants/new",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String restaurantName;
+            String neighborhood;
+            String genre;
+            String favoriteDish;
+            Integer rating;
+            Restaurant newRestaurant = new Restaurant(restaurantName, neighborhood, genre, favoriteDish, rating);
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
     }
 }
