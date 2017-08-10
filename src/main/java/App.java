23b257dd1a19fdb0a73961d45e108ada130636e0
Esti Shay and Kheifer Fuller <kheifer@>
2017-08-10 11:05:50 -0700

    update build.gradle and set up first route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,22 @@
+import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import Food.Restaurant;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
+    public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("restaurants", Restaurant.getInstances());
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine();
+
+
+    }
 }
