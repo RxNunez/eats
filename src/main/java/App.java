eb7a31addd6b6354bcf816aa48e5da0feb21d199
Esti Shay and Kheifer Fuller <esti@>
2017-08-10 14:06:33 -0700

    add forms to create and modify posts

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import Food.Restaurant;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,31 +11,39 @@ public class App {
     public static void main(String[] args) {
 
         staticFileLocation("/public");
-
+        //get: homepage/displays all posts
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Restaurant> listOfPlaces = Restaurant.getInstances();
             model.put("restaurants", Restaurant.getInstances());
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
-
-        get("/restaurants/:id", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            Restaurant place = Restaurant.findById(Integer.parseInt(request.params(":id")));
-            model.put("place", place);
-            return new ModelAndView(model, "restaurant-detail.hbs");
-        }, new HandlebarsTemplateEngine());
-
+        //get: show form to enter new post
         get("/restaurants/new",(request, response) -> {
             Map<String, Object> model = new HashMap<>();
-            String restaurantName;
-            String neighborhood;
-            String genre;
-            String favoriteDish;
-            Integer rating;
+            return new ModelAndView(model,"restaurant-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        //post: process new post form
+        post("/restaurants/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Restaurant> listOfPlaces = Restaurant.getInstances();
+            String restaurantName = request.queryParams("restaurantName");
+            String neighborhood = request.queryParams("neighborhood");
+            String genre = request.queryParams("genre");
+            String favoriteDish = request.queryParams("favoriteDish");
+            Integer rating= Integer.parseInt(request.queryParams("rating"));
             Restaurant newRestaurant = new Restaurant(restaurantName, neighborhood, genre, favoriteDish, rating);
+            model.put("restaurants", listOfPlaces);
             return new ModelAndView(model,"index.hbs");
         }, new HandlebarsTemplateEngine());
-
-
+        //get: Edit entry
+        get("/restaurants/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            int idOfPlace = Integer.parseInt(request.params("id"));
+            Restaurant editPlace = Restaurant.findById(idOfPlace);
+            model.put("editPlace", editPlace);
+            return new ModelAndView(model, "restaurant-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        //
     }
 }
